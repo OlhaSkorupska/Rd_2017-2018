d7a1ca451c27aa26bb48b42962f14471672abceb
Olha Skorupska <olha.serebriakova@gmail.com>
2017-12-20 13:32:27 +0200

    HW12_Functions: Added task_2 (2 variant)

--- task_2/js/task.js
+++ task_2/js/task.js
@@ -4,64 +4,79 @@ function showCalculateMessage(calculateMessage) {
     msgElem.innerHTML = calculateMessage;
 }
 
-function isFindBill(arrayPurse, findBill) {
-    return arrayPurse.indexOf(findBill);
+function compareNumeric(a, b) {
+    return (a > b) ? 1 : -1;
+}
+
+function isSubset(purse, array) {
+    purse.sort(compareNumeric);
+    array.sort(compareNumeric);
+    for (let i = 0; i < array.length; i++) {
+        if (purse[i] !== array[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function findValue(array, value) {
+    let index = -1;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            index = i;
+        }
+    }
+    return index;
+}
+
+function spliceValue(purse, array) {
+    for (let i = 0; i < array.length; i++) {
+        let index = findValue(purse, array[i]);
+        if (index !== -1) {
+            purse.splice(index, 1);
+        }
+    }
+    return purse;
 }
 
 function tickets(queue) {
     let arrayPurse = [];
     let ticketPrice = 25;
-    let valid = 'NO';
-    let deleteIndex = 0;
 
-    for (let i = 0; i <= queue.length; i++) {
+    queue.sort(compareNumeric);
+
+    for (let i = 0; i < queue.length; i++) {
         if (queue[i] === ticketPrice) {
             arrayPurse.push(queue[i]);
         } else if (queue[i] === 50) {
-            deleteIndex = arrayPurse.indexOf(25);
-            if (deleteIndex !== -1) {
-                arrayPurse.splice(deleteIndex, 1);
-                valid = 'YES';
+            if (isSubset(arrayPurse, [25])) {
+                arrayPurse = spliceValue(arrayPurse, [25]);
                 arrayPurse.push(queue[i]);
             }
         } else if (queue[i] === 100) {
-            deleteIndex = arrayPurse.indexOf(50);
-            if (deleteIndex !== -1) {
-                arrayPurse.splice(deleteIndex, 1);
-                valid = 'NO';
-                deleteIndex = arrayPurse.indexOf(25);
-                if (deleteIndex !== -1) {
-                    arrayPurse.splice(deleteIndex, 1);
-                    valid = 'YES';
-                    arrayPurse.push(queue[i]);
-                }                
-            } else {
-                deleteIndex = arrayPurse.indexOf(25);
-                if (deleteIndex !== -1) {
-                    arrayPurse.splice(deleteIndex, 1);
-                    valid = 'NO';
-                    deleteIndex = arrayPurse.indexOf(25);
-                    if (deleteIndex !== -1) {
-                        arrayPurse.splice(deleteIndex, 1);
-                        valid = 'NO';
-                        deleteIndex = arrayPurse.indexOf(25);
-                        if (deleteIndex !== -1) {
-                            arrayPurse.splice(deleteIndex, 1);
-                            valid = 'YES';
-                            arrayPurse.push(queue[i])
-                        }
-                    }
-                }
+            if (isSubset(arrayPurse, [25, 50])) {
+                arrayPurse = spliceValue(arrayPurse, [25, 50]);
+                arrayPurse.push(queue[i]);
+            } else if (isSubset(arrayPurse, [25, 25, 25])) {
+                arrayPurse = spliceValue(arrayPurse, [25, 25, 25]);
+                arrayPurse.push(queue[i]);
             }
         }
     }
-    alert(valid);
-    return valid;
+    let cashbox = arrayPurse.reduce(function (sum, current) {
+        return sum + current;
+    }, 0);
+
+    let result = (cashbox / queue.length === ticketPrice)
+        ? 'YES'
+        : 'NO';
+    showCalculateMessage(result);
+    return result;
 }
 
 function start() {
-    tickets([25, 25, 50, 100]); // YES
-    // tickets([25, 50, 25, 100]); // NO
     // tickets([25, 25, 50, 100]); // YES
+    // tickets([25, 50, 25, 100]); // YES
+    tickets([25, 25, 50, 100, 50]); // NO
     // tickets([25, 50, 100]); // NO
 }
