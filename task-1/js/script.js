edb2fee1e1a93a1b28a3223b3ea846f8b3add169
Olha Skorupska <olha.serebriakova@gmail.com>
2017-12-25 17:51:28 +0200

    HW14_DOM_Date: added slider in task-1

--- task-1/js/script.js
+++ task-1/js/script.js
@@ -1,9 +1,32 @@
-let slides = document.querySelectorAll('.slides__item');
-let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 2000);
+function slider() {
+    let slides = document.querySelectorAll('.slides__item');
+    let currentSlide = 0;
+    let buttons = document.querySelectorAll('.list__button');
+    let slidesWrapper = document.querySelector('.slides');
+    let widthWrapperSlider = 0;
+    let currentSlideWidth = slides[0].offsetWidth;
+    let countSliderWidth = currentSlideWidth;
 
-function nextSlide() {
-    slides[currentSlide].className = 'slides__item';
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('slides__item--showing');
+    for (let i = 0; i <= slides.length - 1; i++) {
+        widthWrapperSlider = widthWrapperSlider + slides[i].offsetWidth;
+    }
+    slidesWrapper.style.width = widthWrapperSlider + 'px';
+
+    function slideShow() {
+        if (countSliderWidth > (widthWrapperSlider - 985)) {
+            countSliderWidth = 0;
+            slidesWrapper.style.transform = 'translate3d(' + countSliderWidth + 'px, 0px, 0px)';
+            countSliderWidth = countSliderWidth + currentSlideWidth;
+        } else {
+            slidesWrapper.style.transform = 'translate3d(' + -countSliderWidth + 'px, 0px, 0px)';
+            countSliderWidth = countSliderWidth + currentSlideWidth;
+        }
+
+        buttons[currentSlide].classList.remove('list__button--check');
+        currentSlide = (currentSlide + 1) % slides.length;
+        buttons[currentSlide].classList.add('list__button--check');
+    }
+    setInterval(slideShow, 2000);
 }
+
+slider();
