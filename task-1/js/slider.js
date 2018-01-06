e9c36a1b73d574267698f2d2f20be5f504652f6d
Olha Skorupska <olha.serebriakova@gmail.com>
2018-01-06 17:01:48 +0200

    HW15_Events: added first part - reverse loop

--- task-1/js/slider.js
+++ task-1/js/slider.js
@@ -1,43 +1,10 @@
-function showSlider() {
-    let slides = document.querySelectorAll('.slides__item');
-    let currentSlide = 0;
-    let buttons = document.querySelectorAll('.list__button');
-    let slidesWrapper = document.querySelector('.slides');
-
-    function slideShow() {
-        // slidesWrapper.style.transform = 'translate3d(' + -985 + 'px, 0px, 0px)';
-
-        buttons[currentSlide].classList.remove('list__button--check');
-        currentSlide = (currentSlide + 1) % slides.length;
-        slides[currentSlide].classList.add('active');
-        buttons[currentSlide].classList.add('list__button--check');
-        let elem = document.querySelectorAll('.slides__item')[0];
-        slidesWrapper.appendChild(elem);
-        slides[currentSlide].classList.remove('active');
-    }
-    setInterval(slideShow, 2000);
-    // let currentSlide = 2;
-    /* function slideShow() {
-        // slidesWrapper.style.transform = 'translate3d(' + -985 + 'px, 0px, 0px)';
-        // buttons[currentSlide + 1].classList.remove('list__button--check');
-        let elem = document.querySelectorAll('.slides__item')[2];
-        slidesWrapper.insertBefore(elem, slidesWrapper.firstChild);
-
-        buttons[currentSlide].classList.add('list__button--check');
-        // buttons[currentSlide].classList.remove('list__button--check');
-        currentSlide = (currentSlide - 1) % slides.length;
-    }
-    setInterval(slideShow, 2000); */
-}
-
-// showSlider();
-
 class Slider {
     constructor(root, options) {
         this.state = {
             classItem: options.classItem || 'slides__item',
             classButton: options.classButton || 'list__button',
             classItemActive: options.classItemActive || 'active',
+            classItemActiveReverse: options.classItemActive || 'active--reverse',
             classButtonActive: options.classButtonActive || 'list__button--check',
             slideTimeout: options.slideTimeout || 2000,
             reverse: options.reverse || false
@@ -51,12 +18,20 @@ class Slider {
     }
 
     activate(slide, button) {
-        slide.classList.add(this.state.classItemActive);
+        if (this.state.reverse) {
+            slide.classList.add(this.state.classItemActiveReverse);
+        } else {
+            slide.classList.add(this.state.classItemActive);
+        }
         button.classList.add(this.state.classButtonActive);
     }
 
     deactivate(slide, button) {
-        slide.classList.remove(this.state.classItemActive);
+        if (this.state.reverse) {
+            slide.classList.remove(this.state.classItemActiveReverse);
+        } else {
+            slide.classList.remove(this.state.classItemActive);
+        }
         button.classList.remove(this.state.classButtonActive);
     }
 
@@ -82,11 +57,13 @@ class Slider {
         let nextSlideIndex;
         if (inverse) {
             nextSlideIndex = (amount + currentSlideIndex - 1) % amount;
+            this.counter = nextSlideIndex - 1;
         } else {
             nextSlideIndex = (amount + currentSlideIndex + 1) % amount;
         }
         this.deactivate(this.slides[currentSlideIndex], this.buttons[currentSlideIndex]);
-        setTimeout(() => (this.activate(this.slides[nextSlideIndex], this.buttons[nextSlideIndex])));
+        setTimeout(() => (this.activate(this.slides[nextSlideIndex],
+            this.buttons[nextSlideIndex])));
     }
 
     start() {
@@ -101,6 +78,6 @@ class Slider {
 
 window.onload = function () {
     let slider = new Slider(document.querySelector('.main__wrapper-slider'),
-        {reverse: false});
+        {reverse: true});
     slider.start();
 };
