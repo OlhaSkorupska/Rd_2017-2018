41ae1e061c8d6380297bd4be07c799b137245b44
Olha Skorupska <olha.serebriakova@gmail.com>
2018-01-06 15:21:57 +0200

    HW15_Events: added second part without validation

--- task-1/js/table.js
+++ task-1/js/table.js
@@ -7,14 +7,13 @@ function getOperation(i, j, operation) {
         case '*':
             return i * j;
         case '/':
-            return i / j;
+            return (i / j).toFixed(2);
         default:
             return i + j;
     }
 }
 
 function dynamicCreationElements(rows = 2, colls = 2, operation = '+') {
-
     let fragment = document.createDocumentFragment();
     let table = document.createElement('table');
     table.classList.add('table');
@@ -27,24 +26,60 @@ function dynamicCreationElements(rows = 2, colls = 2, operation = '+') {
             let cell = document.createElement('td');
             cell.classList.add('table__cell');
             row.appendChild(cell);
-            cell.innerHTML = getOperation(i, j, operation);
+            let result = getOperation(i, j, operation);
+            cell.innerHTML = result;
+            cell.setAttribute('data-tooltip', `${i} ${operation} ${j} = ${result}`);
         }
     }
 
     return fragment.appendChild(table);
 }
 
-window.onload = function () {
-    function handler() {
-        let wrapper = document.getElementsByClassName('wrapper')[0];
-        let rows = document.getElementsByClassName('form__rowsInput')[0].value;
-        let colls = document.getElementsByClassName('form__collsInput')[0].value;
-        let operation = document.getElementsByClassName('form__operationInput')[0].value;
-        // validation();
-        wrapper.appendChild(dynamicCreationElements(rows, colls, operation));
-    }
+function isOperationMark(mark) {
+    return mark.match(/[+\-*/]{1}/g);
+}
 
+let selectedTd;
+
+function showTooltip(target) {
+    if (selectedTd) {
+        selectedTd.classList.remove('tooltip');
+    }
+    selectedTd = target;
+    selectedTd.classList.add('tooltip');
+}
+
+function handlerCell(e) {
+    let target = e.target;
+    if (target.tagName !== 'TD') {
+        return;
+    }
+    showTooltip(target);
+}
+
+function handlerButton(e) {
+    let wrapper = document.getElementsByClassName('wrapper')[0];
+    let rows = document.getElementsByClassName('form__rowsInput')[0].value;
+    let colls = document.getElementsByClassName('form__collsInput')[0].value;
+    let operation = document.getElementsByClassName('form__operationInput')[0].value;
+    isOperationMark(operation);
+    if (!isOperationMark(operation)) {
+        e.preventDefault();
+        wrapper.innerHTML = 'Введите корректные данные';
+    } else {            
+        // validation();
+        if (wrapper.hasChildNodes()) {
+            let child = wrapper.children[0];
+            child.remove();
+        }
+        wrapper.appendChild(dynamicCreationElements(rows, colls, operation));
+        let table = document.getElementsByClassName('table')[0];
+        table.addEventListener('mouseover', handlerCell, false);
+    }
+}
+
+window.addEventListener('load', function () {
     let buttonSend = document.getElementsByClassName('form__buttonSend')[0];
-    buttonSend.addEventListener('click', handler, false);
-};
+    buttonSend.addEventListener('click', handlerButton, false);
+}, false);
 
