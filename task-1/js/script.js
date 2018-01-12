295d98ac3bbf2846d8d3b64465d0617d7f5d6bc6
Olha Skorupska <olha.serebriakova@gmail.com>
2018-01-12 18:55:30 +0200

    HW16_ES6: task-1 (bad second vsriant)

--- task-1/js/script.js
+++ task-1/js/script.js
@@ -1,21 +1,112 @@
-let interest = 100;
-let elem = document.getElementsByClassName('progressBar')[0];
-let positive = document.getElementsByClassName('positive')[0];
+let buttonFirst = document.getElementsByClassName('button')[0];
+let buttonSecond = document.getElementsByClassName('button')[1];
 
-function go() {
-    if (interest !== 0) {
-        interest--;
-        elem.innerHTML = interest + ' %';
-        positive.style.width = interest + ' %';
-    } else {
-        elem.innerHTML = 'Загрузка завершена';
-    }
+let images = document.getElementsByClassName('movies__img');
+let articles = document.getElementsByClassName('movies__article');
+let headers = document.getElementsByClassName('movies__header');
+let items = document.getElementsByClassName('movies__item');
+let bar = document.getElementsByClassName('progressBar__load')[0];
+
+function handler(set, className) {
+    let nameOfClass = className || 'transition-item';
+    set.classList.add(nameOfClass);
 }
 
-let timer = setInterval(function () {
-    go();
-}, 10);
+function animationDone(elem, resolve) {
+    elem.removeEventListener('transitionend', animationDone);
+    resolve();
+}
 
-setTimeout(function () {
-    clearInterval(timer);
-}, 2000);
+let progress = function (elem) {
+    return new Promise((resolve, reject) => {
+        elem.addEventListener('transitionend',
+            (e) => animationDone(elem, resolve),
+            false);
+        elem.style.width = '0';
+    });
+};
+
+let animateSecond = function (elem, className) {
+    return new Promise((resolve, reject) => {
+        elem.addEventListener('transitionend',
+            (e) => animationDone(elem, resolve),
+            false);
+        handler(elem, className);
+    });
+};
+
+let animate = function (elems, className) {
+    const listeners = [];
+    for (const elem of elems) {
+        const listener = new Promise((resolve, reject) => {
+            elem.addEventListener('transitionend', () => {
+                resolve();
+            });
+            handler(elem, className);
+        });
+        listeners.push(listener);
+    }
+    return Promise.all(listeners)
+        .then(() => {
+            Promise.resolve();
+        });
+};
+
+let startFirst = function () {
+    progress(bar)
+        .then(function () {
+            return animate(images, 'transition');
+        })
+        .then(function () {
+            return animate(articles);
+        })
+        .then(function () {
+            return animate(headers);
+        })
+        .then(function () {
+            return animate(items);
+        });
+};
+
+let startSecond = function () {
+    progress(bar)
+        .then(function () {
+            return animateSecond(images[0], 'transition');
+        })
+        .then(function () {
+            return animateSecond(articles[0]);
+        })
+        .then(function () {
+            return animateSecond(headers[0]);
+        })
+        .then(function () {
+            return animateSecond(items[0]);
+        })
+        .then(function () {
+            return animateSecond(images[1], 'transition');
+        })
+        .then(function () {
+            return animateSecond(articles[1]);
+        })
+        .then(function () {
+            return animateSecond(headers[1]);
+        })
+        .then(function () {
+            return animateSecond(items[1]);
+        })
+        .then(function () {
+            return animateSecond(images[2], 'transition');
+        })
+        .then(function () {
+            return animateSecond(articles[2]);
+        })
+        .then(function () {
+            return animateSecond(headers[2]);
+        })
+        .then(function () {
+            return animateSecond(items[2]);
+        });
+};
+buttonFirst.addEventListener('click', startFirst, false);
+buttonSecond.addEventListener('click', startSecond, false);
+
