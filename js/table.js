26e638857b5e02cf432feddd0d0af94a2d32f274
Olha Skorupska <olha.serebriakova@gmail.com>
2018-02-02 09:55:26 +0200

    HW20_Tools: Added webpack.config.js

---
+++ js/table.js
@@ -0,0 +1,85 @@
+import '../sass/table.scss';
+function getOperation(i, j, operation) {
+    switch (operation) {
+        case '+':
+            return i + j;
+        case '-':
+            return i - j;
+        case '*':
+            return i * j;
+        case '/':
+            return (i / j).toFixed(2);
+        default:
+            return i + j;
+    }
+}
+
+function dynamicCreationElements(rows = 2, colls = 2, operation = '+') {
+    let fragment = document.createDocumentFragment();
+    let table = document.createElement('table');
+    table.classList.add('table');
+
+    for (let i = 1; i <= rows; i++) {
+        let row = document.createElement('tr');
+        table.appendChild(row);
+
+        for (let j = 1; j <= colls; j++) {
+            let cell = document.createElement('td');
+            cell.classList.add('table__cell');
+            row.appendChild(cell);
+            let result = getOperation(i, j, operation);
+            cell.innerHTML = result;
+            cell.setAttribute('data-tooltip', `${i} ${operation} ${j} = ${result}`);
+        }
+    }
+
+    return fragment.appendChild(table);
+}
+
+let selectedTd;
+
+function showTooltip(target) {
+    if (selectedTd) {
+        selectedTd.classList.remove('tooltip');
+    }
+    selectedTd = target;
+    selectedTd.classList.add('tooltip');
+}
+
+function handlerCell(e) {
+    let target = e.target;
+    if (target.tagName !== 'TD') {
+        return;
+    }
+    showTooltip(target);
+}
+
+function validation(rows, colls, operation) {
+    return (rows !== '' && rows > 0) &&
+        (colls !== '' && colls > 0) && operation !== '';
+}
+
+function handlerButton(e) {
+    let wrapper = document.getElementsByClassName('wrapper')[0];
+    let rows = document.getElementsByClassName('form__rowsInput')[0].value;
+    let colls = document.getElementsByClassName('form__collsInput')[0].value;
+    let operation = document.getElementsByClassName('form__operationInput')[0].value;
+    if (!validation(rows, colls, operation)) {
+        e.preventDefault();
+        wrapper.innerHTML = 'Введите корректные данные';
+    } else {
+        if (wrapper.children[0]) {
+            wrapper.children[0].remove();
+        }
+        wrapper.textContent = '';
+        wrapper.appendChild(dynamicCreationElements(rows, colls, operation));
+        let table = document.getElementsByClassName('table')[0];
+        table.addEventListener('mouseover', handlerCell, false);
+    }
+}
+
+window.addEventListener('load', function () {
+    let buttonSend = document.getElementsByClassName('form__buttonSend')[0];
+    buttonSend.addEventListener('click', handlerButton, false);
+}, false);
+
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+let extractPlugin = new ExtractTextPlugin({
+    filename: 'css/[name].css'
+});
+
+module.exports = {
+    entry: './js/table.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].js'
+    },
+    watch: true,
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env']
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/i,
+                loader: 'file-loader',
+                query: {
+                    name: '[name].[ext]',
+                    outputPath: 'images/'
+                }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: '10000',
+                    name: '[name].[ext]',
+                    outputPath: 'fonts/'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: [{
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'sass-loader'
+                    }],
+                    fallback: 'style-loader'
+                })
+            },
+            {
+                test: /\.(html)$/,
+                exclude: /node_modules/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    plugins: [
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            title: 'table',
+            template: './table.html',
+            filename: 'table.html'
+        })
+    ]
+};
